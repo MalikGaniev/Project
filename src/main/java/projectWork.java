d3cfdb38d6266961ff671cee8d7a5d2af2679033
Malik_Git <malikganiev2002@gmail.com>
2023-02-13 11:40:41 -0500

    Changes getter

---
+++ src/main/java/projectWork.java
@@ -0,0 +1,27 @@
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.LinkedHashMap;
+
+public class projectWork {
+    @Getter
+   private Integer m;
+   @Getter
+   private LinkedHashMap<BigDecimal,Boolean> t;
+
+   protected static String k;
+
+
+
+    public projectWork(Integer m){
+
+    }
+    private void setM (Integer m){
+
+        if(m < -18){
+            throw new RuntimeException();
+        }else{
+            this.m = m;
+        }
+    }
+}
