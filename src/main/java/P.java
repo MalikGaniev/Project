b5b5cc23936d3d921e0be6500c59e02ca6190cab
Malik_Git <malikganiev2002@gmail.com>
2023-02-13 11:49:54 -0500

    Changes abstract

---
+++ src/main/java/P.java
@@ -0,0 +1,6 @@
+import java.util.Set;
+
+public abstract class P {
+    public abstract Set<Character> k(char[] l);
+
+}
---
+++ src/main/java/S.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class S extends P{
+    @Override
+    public Set<Character> k(char[] l) {
+        Set<Character> e =new HashSet<>();
+
+        for ( char x:l ) {
+            e.add(x);
+        }
+        return e;
+    }
+}
