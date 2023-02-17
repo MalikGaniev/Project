1f9070fbd6d100ee8075b35c25027262d9ce1bf7
Malik_Git <malikganiev2002@gmail.com>
2023-02-17 11:17:10 -0500

    Changes

---
+++ src/main/java/r.java
@@ -0,0 +1,15 @@
+import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.List;
+
+public class r {
+    public static void main(String[] args) {
+        func f=new func() {
+            @Override
+            public LinkedList<BigDecimal> A(List<Long> k) {
+                return null;
+            }
+        };
+        func f1=k -> new LinkedList<>();
+    }
+}
